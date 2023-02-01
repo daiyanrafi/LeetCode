5da5233f7c0af6faf0733a6d18fa6c5b5042bcd7
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-02-01 13:55:36 +0600

    012 1355

--- 26_removeDupli.js
+++ 26_removeDupli.js
@@ -1,12 +1,24 @@
 // 26. Remove Duplicates from Sorted Array
 
 var removeDuplicates = function (nums) {
-    let index = 1;
-    for (let i=0; i < nums.length -1; i++) {
-        if(nums[i] !== nums[i+1]){
-            nums[index] = nums[i+1]
-            index++
-        }
+  let index = 1;
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] !== nums[i + 1]) {
+      nums[index] = nums[i + 1];
+      index++;
     }
-    return index;
+  }
+  return index;
+};
+
+//another solution
+
+var removeDuplicates = function (nums) {
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        nums.splice(i, 1);
+      }
+    }
+  }
 };
---
+++ 27_removeElement.js
@@ -0,0 +1,19 @@
+var removeElement = function (nums, val){
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === val){
+            nums.splice(i, 1);
+            i--;
+        }
+    }
+    
+}
+
+//another approach
+
+var removeElement = function (nums, val) {
+    for(let i = num.length - 1; i >= 0; i--){
+        if(nums[i] === val){
+            nums.splice(i, 1);
+        }
+    }
+}
