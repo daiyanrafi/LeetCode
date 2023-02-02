ec4274ae99b13240babfc2610b0519014e4c915f
Daiyan Rafi <daiyanrafi1@gmail.com>
2023-02-02 14:55:52 +0600

    0202 1455

--- 27_removeElement.js
+++ 27_removeElement.js
@@ -1,19 +1,18 @@
-var removeElement = function (nums, val){
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] === val){
-            nums.splice(i, 1);
-            i--;
-        }
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
     }
-    
-}
+  }
+};
 
 //another approach
 
 var removeElement = function (nums, val) {
-    for(let i = num.length - 1; i >= 0; i--){
-        if(nums[i] === val){
-            nums.splice(i, 1);
-        }
+  for (let i = num.length - 1; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
     }
-}
+  }
+};
